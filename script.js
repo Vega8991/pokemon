47440bfdf60b288c69b87096535c68cb89619049
Vega8991 <manuelvega011204@gmail.com>
2025-01-14 14:16:39 +0100

    script updated

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const form = document.getElementById
+const form = document.getElementById("pokemon-form");
+const input = document.getElementById("pokemon-name");
+const pokemonInfoDiv = document.getElementById("pokemon-info");
+
+async function fetchPokemonData(pokemonName) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
+        if (!response.ok) {
+            throw new Error("Pokemon no encontrado");
+        }
+
+        const data = await response.json();
+        displayPokemonInfo(data);
+    } catch (error) {
+        pokemonInfoDiv.innerHTML = `<p>${error.message}</p>`;
+        pokemonInfoDiv.style.display = "block";
+    }
+}
